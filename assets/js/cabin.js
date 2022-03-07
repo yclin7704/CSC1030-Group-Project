f3799dfeabe57aab904f41852654228514e2a9a4
James McKee <56560271+James-McK@users.noreply.github.com>
2022-03-07 22:56:42 +0000

    Comments

--- assets/js/cabin.js
+++ assets/js/cabin.js
@@ -22,18 +22,34 @@ async function main() {
 	runEvent("firstVisitOutside");
 }
 
+/**
+ * Run the event with the given ID
+ * @param {string} eventId The event ID
+ */
 async function runEvent(eventId) {
 	let eventData = getEventData(eventId);
+	// Update the game's state, if needed
 	updateGameState(eventData.stateChanges);
+	// Display the event text
 	print(eventData.text);
+	// Update the setting image
 	setImg(eventData.img);
+	// Show the user's options
 	setChoices(eventData.optsId);
 }
 
+/**
+ *
+ * @param {string} eventId The event ID
+ * @returns The event data, partially overriden by the profession event data if applicable
+ */
 function getEventData(eventId) {
+	// Get the matching base event data
 	let eventData = events.find((event) => event.id === eventId);
+	// Get the matching profession event data, if it exists
 	let profEventData = getProfEventData().find((event) => event.id === eventId);
 
+	// If the profession data doesn't exist, then you can just return the base data
 	if (profEventData === undefined) return eventData;
 	else {
 		let data = {};
@@ -42,14 +58,20 @@ function getEventData(eventId) {
 			data[key] = eventData[key];
 		}
 
+		// Override the base data with any existing profession event data
 		for (let key in profEventData) {
 			data[key] = profEventData[key];
 		}
 
+		// Return the result
 		return data;
 	}
 }
 
+/**
+ * Get the override event data for the current profession
+ * @returns The event data for the current profession
+ */
 function getProfEventData() {
 	switch (profession) {
 		case profHunter:
@@ -65,12 +87,20 @@ function getProfEventData() {
 	}
 }
 
+/**
+ * Update the game state with the given changes
+ * @param {JSON} changes Changes to be made
+ */
 function updateGameState(changes) {
 	for (let key in changes) {
 		gameState[key] = changes[key];
 	}
 }
 
+/**
+ * Display the options from the given set
+ * @param {string} optsId The ID of the set of options to display
+ */
 async function setChoices(optsId) {
 	const choiceDiv = getDOM("divChoices");
 	// Clear previous options
@@ -93,15 +123,31 @@ async function setChoices(optsId) {
 	}
 }
 
+/**
+ * Called when the user picks an option by clicking on the corresponding button
+ * @param {JSON} opt The chosen option's data
+ */
 async function selectOption(opt) {
+	// Update game state if needed
 	updateGameState(opt.stateChanges);
+	// Continue to the next event
 	let nextEventId = opt.nextEventId;
 	runEvent(nextEventId);
 }
 
+/**
+ * Check if all the given requirements are met
+ * @todo Any way of doing (A OR B)?
+ * @param {JSON} reqs The requirements to check
+ * @returns True if all requirements are met, false if not
+ */
 function areReqsMet(reqs) {
+	// Assume all requirements are met by default
 	let allMet = true;
+	// Iterate through the requirements
 	for (let key in reqs) {
+		// If all previous requirements were met, and
+		// (the current requirement is explicitly met OR the value is supposed to be false and has not yet been defined in gameState)
 		allMet = allMet && (reqs[key] === gameState[key] || (!reqs[key] && !gameState[key]));
 	}
 	return allMet;
@@ -109,6 +155,7 @@ function areReqsMet(reqs) {
 
 /**
  * Display the given dialogue to the user
+ * @todo Some fancy ttyping effect maybe?
  * @param {string} text The text to display
  */
 async function print(text) {
@@ -116,6 +163,10 @@ async function print(text) {
 	dialogueBox.innerHTML = text;
 }
 
+/**
+ * Sets the location image
+ * @param {string} src The location of the image file
+ */
 async function setImg(src) {
 	getDOM("imgLocation").src = src;
 }
