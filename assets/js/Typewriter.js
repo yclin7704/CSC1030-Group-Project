27cc5215bb44aa2de5153f628758fe6af56e0e46
RingLord54 <andrew.moneypenny@btinternet.com>
2022-03-07 22:18:22 +0000

    Comments Updated for the Typewriter.js

--- assets/js/Typewriter.js
+++ assets/js/Typewriter.js
@@ -1,4 +1,4 @@
-// Will display the textNode text by printing it in a typewriter liek fashion
+// Will display the textNode text by printing it in a typewriter like fashion
 async function typeSentence(sentence, delay = 30) {
 
     // Clears the HTML so that it doesn't keep adding on to it
@@ -7,7 +7,10 @@ async function typeSentence(sentence, delay = 30) {
     // splits the sentence into individual characters
     var letters = sentence.split(""); 
 
+    // acts as an index position
     let i = 0;
+
+    // Loops through each character in the split sentence
     while(i < letters.length) {
 
         // the delay before each letter is printed
@@ -19,6 +22,8 @@ async function typeSentence(sentence, delay = 30) {
             i++;
         }
         else {
+
+            //creates a new string to store the value of the HTML tag to check later
             var tag = letters[i];
             i++;
             while(letters[i] != ">"){
@@ -28,7 +33,10 @@ async function typeSentence(sentence, delay = 30) {
             tag += letters[i];
             i++;
             switch(tag){
+                // If the tag is a <strong> tag this code will run
                 case "<strong>": i=tagStrong(letters, i); break;
+
+                // This code will run by default for tags like <br> as they don't have a corresponding closing tag
                 default: document.getElementById('DialogueHospital').innerHTML += tag;
             }
         }
@@ -37,8 +45,7 @@ async function typeSentence(sentence, delay = 30) {
 }
 
 
-
-
+// Will allow the Typewriter effect to print the text with the <strong></strong> tags applied
 function tagStrong(letters, i){
     var word = "";
     while(letters[i] != "<"){
@@ -46,11 +53,10 @@ function tagStrong(letters, i){
         i++;
     }
     i += 9;
-    document.getElementById('DialogueHospital').innerHTML += "<strong>" + word;
+    // Will print the text in between the <strong></strong> tags so the effects are applied
+    document.getElementById('DialogueHospital').innerHTML += "<strong>" + word + "</strong>";
     return i;
 }
-
-
 
 
 // Creates a time delay before each letter is printed
