631309c6e295923af7841ab6b7ae20a8ada559df
James McKee <56560271+James-McK@users.noreply.github.com>
2022-03-13 21:53:19 +0000

    Reset values when loading statistics page

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -7,12 +7,17 @@ async function main() {
 	get("temperatureSpan").innerHTML = getTemp();
 	get("timeSpan").innerHTML = getTime();
 	get("professionSpan").innerHTML = sessionStorage.getItem("profession");
+
+	resetValues();
 }
 
-async function playAgain() {
+async function resetValues() {
 	// Reset temperature etc.
 	setTemp(40, false);
 	saveTime(0);
+}
+
+async function playAgain() {
 	// Redirect to start screen
 	window.location.href = "PlayerMenu.html";
 }
