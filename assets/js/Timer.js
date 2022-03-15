4f5b7334ec723321c574bc0a95ab1744693cf717
James McKee <56560271+James-McK@users.noreply.github.com>
2022-03-15 15:10:12 +0000

    Testing updating time

--- assets/js/Timer.js
+++ assets/js/Timer.js
@@ -1,5 +1,6 @@
 let time = getSavedTime();
 let myTimer;
+let timeSpan = document.getElementById("timeSpan");
 
 function getSavedTime() {
 	let saved = parseInt(sessionStorage.getItem("Time"));
@@ -14,6 +15,47 @@ function StartTimer() {
 function Timer() {
 	time++;
 	saveTime();
+
+	if (timeSpan) {
+		// Time the day starts at (eg 1700 being 5:00pm)
+		const dayStart = 1700;
+		// Time the day ends at (eg 2200 being 10:00pm)
+		const dayEnd = 2200;
+		// Length time of day takes in secconds
+		const dayLength = 250;
+
+		const nightStart = 2200;
+		const nightEnd = 600;
+		const nightLength = 50;
+
+		let dayLen = dayEnd - dayStart;
+
+		let nightLen;
+		if (nightStart < nightEnd) nightLen = nightEnd - nightStart;
+		else nightLen = nightStart + 24 - nightEnd;
+
+		let translatedTime = (dayLen / dayLength) * time;
+
+		let hour, minutes;
+
+		if (translatedTime < dayLen) {
+			hour = Math.floor(translatedTime / 100) + dayStart / 100;
+			minutes = Math.floor(((translatedTime % 100) / 25) * 3);
+		} else {
+			translatedTime = (nightLen / nightLength) * (time - dayLength);
+			hour = Math.floor(translatedTime / 100) + nightStart / 100;
+			minutes = Math.floor(((translatedTime % 100) / 25) * 3);
+		}
+
+		hour = hour % 24;
+
+		timeSpan.innerHTML = `${hour}:${padTime(minutes * 5)}<br />${translatedTime}`;
+	}
+}
+
+function padTime(num) {
+	if (num < 10) return "0" + num;
+	else return num;
 }
 
 function StopTimer() {
