5f80c09bd598b8a9ef145b06ac6e0ce8fb0ee866
Ken-Shek <kshek@hotmail.com>
2022-04-02 17:02:02 +0100

    Update farmHouse.js

--- assets/js/farmHouse.js
+++ assets/js/farmHouse.js
@@ -56,7 +56,8 @@ function getGameState() {
 		return {
 			// TODO: Get profession properly with sessionStorage.getItem("profession");
 			profession: profDoctor,
-		};
+		}
+    }
 
 // This function displays the current text node in the dialogue box. The index of the text node is required as a parameter.
 
